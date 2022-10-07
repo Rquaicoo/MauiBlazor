788d92a113a8460efdb6a8e20258200f95c0f160
Rquaicoo <russellquaicoo1@gmail.com>
2022-10-07 14:55:54 +0000

    dispatch weather data inofrmation

---
+++ MauiBlazor/States/Effects/Effects.cs
@@ -0,0 +1,35 @@
+﻿using Fluxor;
+using MauiBlazor.Data;
+using MauiBlazor.States.Actions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using IDispatcher = Fluxor.IDispatcher;
+
+namespace MauiBlazor.States.Effects
+{
+    internal class Effects
+    {
+        private readonly HttpClient Http;
+
+        public Effects(HttpClient http)
+        {
+            Http = http;
+        }
+
+        [EffectMethod]
+        public async Task HandleFetchDataAction(FetchDataAction action, IDispatcher dispatcher)
+        {
+            var forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+
+            if (forecasts != null)
+            {
+                dispatcher.Dispatch(new FetchDataResultAction(forecasts));
+            }
+     
+        }
+    }
+}
