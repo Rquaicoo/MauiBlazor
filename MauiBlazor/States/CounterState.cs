d5ba34c9883e70781d6892f8875ad45baa1cf65b
Rquaicoo <russellquaicoo1@gmail.com>
2022-10-07 12:28:13 +0000

    state class for counter

---
+++ MauiBlazor/States/CounterState.cs
@@ -0,0 +1,22 @@
+﻿using Fluxor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBlazor.States
+{
+    [FeatureState]
+    internal class CounterState
+    {
+        public int ClickCount { get; }
+        
+        public CounterState(int clickCount)
+        {
+            ClickCount = clickCount;
+        }
+
+        public CounterState() { }
+    }
+}
