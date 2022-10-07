c96bd4f358c986f65f059dbcfb246934659394aa
Rquaicoo <russellquaicoo1@gmail.com>
2022-10-07 14:54:01 +0000

    actions for updating state and getting weather forecast data

---
+++ MauiBlazor/States/Actions/FetchDataResultAction.cs
@@ -0,0 +1,19 @@
+﻿using MauiBlazor.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBlazor.States.Actions
+{
+    internal class FetchDataResultAction
+    {
+        public WeatherForecast[] Forecasts { get; }
+
+        public FetchDataResultAction(WeatherForecast[] forecasts)
+        {
+            Forecasts = forecasts;
+        }
+    }
+}
