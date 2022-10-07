276b3736503ae9c2b202259697bda1c51a919edc
Rquaicoo <russellquaicoo1@gmail.com>
2022-10-07 14:54:46 +0000

    weather infomration reducer

--- MauiBlazor/States/Reducers/Reducers.cs
+++ MauiBlazor/States/Reducers/Reducers.cs
@@ -17,5 +17,13 @@ namespace MauiBlazor.States.Reducers
         [ReducerMethod]
         public static CounterState ReduceResetCounterAction(CounterState state, DecrementCounterAction action) =>
             new(clickCount: state.ClickCount - 1);
+
+        [ReducerMethod]
+        public static WeatherState ReduceFetchDataAction(WeatherState state, FetchDataAction action) =>
+            new(isLoading: true, forecasts: null);
+
+        [ReducerMethod]
+        public static WeatherState ReduceFetchDataResultAction(WeatherState state, FetchDataResultAction action) =>
+            new(isLoading: false, forecasts: action.Forecasts);
     }
 }
