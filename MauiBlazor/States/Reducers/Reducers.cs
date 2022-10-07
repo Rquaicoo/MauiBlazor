b8a18a453f58747c8bc2e58715abdf790313d723
Rquaicoo <russellquaicoo1@gmail.com>
2022-10-07 12:26:40 +0000

    reducer method for increment and decrement actions

---
+++ MauiBlazor/States/Reducers/Reducers.cs
@@ -0,0 +1,21 @@
+﻿using Fluxor;
+using MauiBlazor.States.Actions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBlazor.States.Reducers
+{
+    internal class Reducers
+    {
+        [ReducerMethod]
+        public static CounterState ReduceIncrementCounterAction(CounterState state, IncrementCounterAction action) =>
+            new(clickCount :state.ClickCount + 1);
+
+        [ReducerMethod]
+        public static CounterState ReduceResetCounterAction(CounterState state, DecrementCounterAction action) =>
+            new(clickCount: state.ClickCount - 1);
+    }
+}
