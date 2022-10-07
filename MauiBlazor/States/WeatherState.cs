0fc55306c7eef5bbdf5a9fc98974d64f98c5aa1f
Rquaicoo <russellquaicoo1@gmail.com>
2022-10-07 14:56:32 +0000

    state class for weather information

---
+++ MauiBlazor/States/WeatherState.cs
@@ -0,0 +1,25 @@
+﻿using Fluxor;
+using MauiBlazor.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBlazor.States
+{   
+    [FeatureState]
+    internal class WeatherState
+    {
+        public bool IsLoading { get; }
+        public WeatherForecast[] Forecasts { get; }
+        
+        private WeatherState() { }
+
+        public WeatherState(bool isLoading, WeatherForecast[] forecasts)
+        {
+            IsLoading = isLoading;
+            Forecasts = forecasts ?? Array.Empty<WeatherForecast>();
+        }
+    }
+}
